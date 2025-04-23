bba9a05b78a55c6c1b544d62cc7a2d5a193b73f8
frederik.schocke <frederik.schocke@aoe.com>
2025-04-23 23:34:10 +0200

    feat: add separator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Ingredients from "./components/Ingredients/Ingredients";
 import Recipe from "./components/Recipe/Recipe";
+import Separator from "./components/Separator/Separator";
 import ThemeToggle from "./components/ThemeToggle/ThemeToggle";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <main className="grid gap-4 m-4 justify-items-center">
       <ThemeToggle />
       <Ingredients />
+      <Separator />
       <Recipe />
     </main>
   );
---
+++ src/components/Separator/Separator.tsx
@@ -0,0 +1,9 @@
+const Separator = () => {
+  return (
+    <div className="flex w-sm md:w-md flex-col">
+      <div role="separator" className="divider m-0"></div>
+    </div>
+  );
+};
+
+export default Separator;
