ead5c252982565b74602ffc23f1ddc144c5e9379
frederik.schocke <frederik.schocke@aoe.com>
2025-04-26 14:59:47 +0200

    ci: adjust vite config to github pages deployment path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/broetjim/", // needed for deployment to GitHub pages
   plugins: [react(), tailwindcss()],
 });
