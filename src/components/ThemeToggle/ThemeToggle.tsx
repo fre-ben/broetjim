3215a8f95ecb05025407f7f979ad10d55d63e33e
frederik.schocke <frederik.schocke@aoe.com>
2025-04-30 09:48:14 +0200

    fix: translate english aria-labels on svgs

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -23,7 +23,7 @@ const ThemeToggle = () => {
         />
 
         <svg
-          aria-label="sun"
+          aria-label="Sonne (Heller Modus)"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
         >
@@ -47,7 +47,7 @@ const ThemeToggle = () => {
         </svg>
 
         <svg
-          aria-label="moon"
+          aria-label="Mond (Dunkler Modus)"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
         >
